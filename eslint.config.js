99721194d2873627b2be2cd25dab6a48244aafd4
Rustam <ermatovrus22@gmail.com>
2025-07-11 00:36:22 -0700

    updated dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -21,9 +22,21 @@ export default defineConfig([
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
+    },  
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+     plugins: {
+      react,         // 👈 Add this
+      reactHooks,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }], ...js.configs.recommended.rules,
+...react.configs.recommended.rules,
+...react.configs['jsx-runtime'].rules, 'no-unused-vars': 'warn', //this changes the error to a warning
+        'react/prop-types': 'off',
     },
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const todos = [
+    { id: 1, title: 'Learn React', completed: false },
+    { id: 2, title: 'Build a ToDo App', completed: false },
+    { id: 3, title: 'Share with friends', completed: false }
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>ToDo List</h1>
+      <ul>
+        {
+          todos.map((el) =>{
+            return(
+              <li key = {el.id}>
+                {el.title}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
