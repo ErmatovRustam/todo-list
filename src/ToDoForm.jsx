2d75005d46f66c22d0471031cf54bc842e15c4e4
Rustam <ermatovrus22@gmail.com>
2025-08-05 23:44:08 -0700

    update form for todo list

--- src/ToDoForm.jsx
+++ src/ToDoForm.jsx
@@ -1,9 +1,31 @@
-function ToDoForm() {
+import React, { useRef } from 'react';
+
+function ToDoForm({ onAddTodo }) {
+  const todoTitleInput = useRef(null);
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    const title = event.target.title.value;
+    if (!title.trim()) return; 
+
+    onAddTodo(title);
+
+    event.target.title.value = "";  
+    todoTitleInput.current.focus(); 
+  }
+
   return (
-    <form >
-        <label htmlFor="todoTitle">TODO</label>
-      <input type="text" id = "todoTitle" />
-      <button type="submit"> Add Todo</button>
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle">TODO</label>
+      <input
+        type="text"
+        id="todoTitle"
+        name="title"
+        placeholder="Enter todo..."
+        ref={todoTitleInput}  
+      />
+      <button type="submit">Add Todo</button>
     </form>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import TodoListItem  from "./TodoListItem"
+import TodoListItem from "./TodoListItem";
 
-
-function ToDoList(){
-    const todos = [
-    { id: 1, title: 'Learn React', completed: false },
-    { id: 2, title: 'Build a ToDo App', completed: false },
-    { id: 3, title: 'Share with friends', completed: false }
-  ]
-    return(
-      <ul>
-        {todos.map(el=> (
-          <TodoListItem key = {el.id} todo = {el} />
-        ))}
-      </ul>
-    )
+function ToDoList({ todoList }) { 
+  return (
+    <ul>
+      {todoList.map(todo => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
-export default ToDoList
+export default ToDoList;
